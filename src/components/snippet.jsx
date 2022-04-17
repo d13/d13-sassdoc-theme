b4ec022445025b7cda83d8b29a145304a2cb8e4d
Keith Daulton <kdaulton@d13design.com>
2022-04-17 17:53:50 -0400

    feat(snippet): add language prop
    Fixes #2

--- src/components/snippet.jsx
+++ src/components/snippet.jsx
@@ -6,12 +6,11 @@ import ToggleButton from './toggle-button';
 
 SyntaxHighlighter.registerLanguage('scss', scss);
 
-export default function Snippet({ inline = false, className, preview, children, ...props }) {
+export default function Snippet({ language = 'scss', inline = false, className, preview, children, ...props }) {
   const [expanded, setExpanded] = useState(false);
 
   return (<div {...props} className={classNames('snippet', inline && 'snippet--inline', className)}>
     {preview && <ToggleButton className="snippet__toggle" on={expanded} aria-expanded={expanded} onClick={() => setExpanded(!expanded)}>toggle code block</ToggleButton>}
-    {/* TODO: add support for: css, markup, javascript, bash */}
-    <SyntaxHighlighter language="scss" className="language-scss snippet__block" useInlineStyles={false}>{(!expanded && preview) ? preview : children}</SyntaxHighlighter>
+    <SyntaxHighlighter language={language} className={classNames(`language-${language}`, 'snippet__block')} useInlineStyles={false}>{(!expanded && preview) ? preview : children}</SyntaxHighlighter>
   </div>);
 }
