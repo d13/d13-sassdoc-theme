f4f6431ba4373cc4e775b751b810b3c3b445e054
Keith Daulton <kdaulton@d13design.com>
2022-04-18 20:03:25 -0400

    feat(side-nav-group): lazy render children

--- src/templates/parts/side-nav/side-nav-group.jsx
+++ src/templates/parts/side-nav/side-nav-group.jsx
@@ -6,11 +6,13 @@ import ToggleButton from '../../../components/toggle-button';
 export function SideNavGroup({ name, url = '#', className, children, ...props }) {
   const [expanded, setExpanded] = useState(false);
 
+  const [interacted, setInteracted] = useState(false);
+
   return (
     <li className={classNames('side-nav-group', className)} {...props}>
-      {children && <ToggleButton on={expanded} aria-expanded={expanded} emphasis="minimal" className="side-nav-group__toggle" onClick={() => setExpanded(!expanded)}>toggle {name}</ToggleButton>}
-      <Link href={url}><a className="side-nav-group__name" onClick={() => setExpanded(true)}>{name}</a></Link>
-      {children && <ul className="side-nav-group__items">{children}</ul>}
+      {children && <ToggleButton on={expanded} aria-expanded={expanded} emphasis="minimal" className="side-nav-group__toggle" onClick={() => { setInteracted(true); setExpanded(!expanded); }}>toggle {name}</ToggleButton>}
+      <Link href={url}><a className="side-nav-group__name" onClick={() => { setInteracted(true); setExpanded(true); }}>{name}</a></Link>
+      {interacted && children && <ul className="side-nav-group__items">{children}</ul>}
     </li>
   );
 }
